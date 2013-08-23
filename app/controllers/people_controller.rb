8ac35ce60f9dadc1dd98131346551c465b4167b0
Anders Fisher <atleastimtrying@gmail.com>
2013-08-23 08:23:54 +0100

    adding edit functionality to a person

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,5 +1,5 @@
 class PeopleController < ApplicationController
-  before_action :set_person, only: [:show]
+  before_action :set_person, only: [:show, :update, :edit]
 
   # GET /people
   # GET /people.json
@@ -13,6 +13,24 @@ class PeopleController < ApplicationController
     @food = @person.food_today
   end
 
+  # GET /people/1/edit
+  def edit
+  end
+
+    # PATCH/PUT /people/1
+  # PATCH/PUT /people/1.json
+  def update
+    respond_to do |format|
+      if @person.update(person_params)
+        format.html { redirect_to @person, notice: "#{@person.name} was successfully updated." }
+        format.json { head :no_content }
+      else
+        format.html { render action: 'edit' }
+        format.json { render json: @person.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_person
@@ -21,6 +39,6 @@ class PeopleController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def person_params
-      params.require(:person).permit(:points_per_day, :name, :weight)
+      params.require(:person).permit(:points_per_day, :name)
     end
 end
